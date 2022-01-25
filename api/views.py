15b939bb0e63e549dfbac9d10ff8ebc1aa5aae10
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-01-25 15:02:06 +0900

    API作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework import generics, viewsets
+from .serializers import TaskSerializer, UserSerializer
+from .models import Task
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class MyProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user  # loginしているユーザ情報
+
+
+"""
+create/read/delete/get
+"""
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
